142460dcaafe27633c7b023d1bb41541f10b7a6a
Abhijit Roy <dalvath3700@gmail.com>
2021-05-13 18:05:27 +0530

    bot's input file added

---
+++ app/input.py
@@ -0,0 +1,17 @@
+API_key = "1893739737:AAGAVoUXZ-OB27iYvy9HyyvErtkoDbqB1RA"
+
+# Capture using `$ heroku config | grep REDIS` from the terminal at App's root dir
+REDIS_URL = 'redis://:pc7d51574d2f61adce3d96d5ffd0bd22b56101591aec1293610bba814122ad4a7@ec2-34-200-7-84.compute-1.amazonaws.com:18580'
+
+Chain_URL = 'http://jungle3.cryptolions.io:80'      # Jungle Testnet
+chain_name = 'jungle'
+chain_type = 'eos-testnet'
+
+# kycweosiobot eosio_ac
+kyc_eosio_ac = 'kycweosiobot'
+kyc_ac_private_key = '5KEC6feA46GLZC3u5fifUPui226dByFh7UWmC3fMKdF2F4PJg9J'
+kyc_ac_key_perm = 'active'
+
+# ACTION
+addmod_action = 'addmodkyc'
+del_action = 'delkyc'
