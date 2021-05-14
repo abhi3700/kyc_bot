018a38c42371d1602dceb2e6e395644111ef94c6
Abhijit Roy <dalvath3700@gmail.com>
2021-05-15 03:39:50 +0530

    bot updated

--- app/bot.py
+++ app/bot.py
@@ -1,1 +1,14 @@
-from flask import 
+import botogram
+# import redis
+import json
+import asyncio
+
+# from aioeos import EosAccount, EosJsonRpc, EosTransaction
+# from aioeos import types
+
+# from aioeos.exceptions import EosAccountDoesntExistException
+# from aioeos.exceptions import EosAssertMessageException
+# from aioeos.exceptions import EosDeadlineException
+# from aioeos.exceptions import EosRamUsageExceededException
+# from aioeos.exceptions import EosTxCpuUsageExceededException
+# from aioeos.exceptions import EosTxNetUsageExceededException
